277301f36951fddcb8ef784aee9e721567cf3c30
kamyu <kamyu1537@gmail.com>
2024-10-07 15:14:38 +0900

    add DummyBufferWriter and optimize MemoryPackPacketEncoder

---
+++ Common/Buffer/DummyBufferWriter.cs
@@ -0,0 +1,13 @@
+﻿using System.Buffers;
+
+namespace Common.Buffer;
+
+public class DummyBufferWriter : IBufferWriter<byte>
+{
+    public void Advance(int count)
+    {
+    }
+
+    public Memory<byte> GetMemory(int sizeHint = 0) => throw new NotSupportedException();
+    public Span<byte> GetSpan(int sizeHint = 0) => throw new NotSupportedException();
+}
--- Common/Encoder/MemoryPackPacketEncoder.cs
+++ Common/Encoder/MemoryPackPacketEncoder.cs
@@ -1,4 +1,6 @@
-﻿using DotNetty.Buffers;
+﻿using System.Buffers;
+using Common.Buffer;
+using DotNetty.Buffers;
 using DotNetty.Codecs;
 using DotNetty.Transport.Channels;
 using MemoryPack;
@@ -8,10 +10,41 @@ namespace Common.Encoder;
 
 public class MemoryPackPacketEncoder : MessageToByteEncoder<IPacket>
 {
+    private static readonly MemoryPool<byte> MemoryPool = MemoryPool<byte>.Shared;
+    private static readonly DummyBufferWriter Writer = new();
+
     protected override void Encode(IChannelHandlerContext context, IPacket message, IByteBuffer output)
     {
         ArgumentNullException.ThrowIfNull(message);
-        var bytes = MemoryPackSerializer.Serialize(message);
-        output.WriteBytes(bytes);
+
+        var dummyBufferWriter = Writer;
+        using var memoryOwner = MemoryPool.Rent();
+        using var memoryPackWriterState = MemoryPackWriterOptionalStatePool.Rent(null);
+
+        var memory = memoryOwner.Memory;
+        var writer = new MemoryPackWriter<DummyBufferWriter>(ref dummyBufferWriter, memory.Span, memoryPackWriterState);
+        
+        try
+        {
+            writer.WriteValue(message);
+            var length = writer.WrittenCount;
+            
+            if (output.HasArray)
+            {
+                output.EnsureWritable(length);
+                var startIndex = output.ArrayOffset + output.WriterIndex;
+                memory.Span[..length].CopyTo(output.Array.AsSpan(startIndex, length));
+                output.SetWriterIndex(output.WriterIndex + length);
+            }
+            else
+            {
+                Console.WriteLine("MemoryPackPacketEncoder: output does not have array");
+                output.WriteBytes(memory.Span[..length].ToArray());
+            }
+        }
+        finally
+        {
+            writer.Flush();
+        }
     }
 }
