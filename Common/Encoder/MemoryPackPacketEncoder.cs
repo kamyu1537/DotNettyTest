47278ed4ca85e2023cf93a3750488d4e36021894
kamyu <kamyu1537@gmail.com>
2024-10-07 14:26:44 +0900

    remove DummyBufferWriter, simplify packet encoding

--- Common/Encoder/MemoryPackPacketEncoder.cs
+++ Common/Encoder/MemoryPackPacketEncoder.cs
@@ -1,6 +1,4 @@
-﻿using System.Buffers;
-using Common.Buffer;
-using DotNetty.Buffers;
+﻿using DotNetty.Buffers;
 using DotNetty.Codecs;
 using DotNetty.Transport.Channels;
 using MemoryPack;
@@ -10,29 +8,10 @@ namespace Common.Encoder;
 
 public class MemoryPackPacketEncoder : MessageToByteEncoder<IPacket>
 {
-    private static readonly MemoryPool<byte> MemoryPool = MemoryPool<byte>.Shared;
-    private static readonly DummyBufferWriter Writer = new();
-    
     protected override void Encode(IChannelHandlerContext context, IPacket message, IByteBuffer output)
     {
         ArgumentNullException.ThrowIfNull(message);
-
-        var dummy = Writer;
-        using var memoryOwner = MemoryPool.Rent();
-        using var state = MemoryPackWriterOptionalStatePool.Rent(null);
-        
-        var memory = memoryOwner.Memory;
-        var writer = new MemoryPackWriter<DummyBufferWriter>(ref dummy, memory.Span, state);
-
-        try
-        {
-            writer.WriteValue(message);
-            var length = writer.WrittenCount;
-            output.WriteBytes(memory.ToArray(), 0, length);
-        }
-        finally
-        {
-            writer.Flush();    
-        }
+        var bytes = MemoryPackSerializer.Serialize(message);
+        output.WriteBytes(bytes);
     }
 }
